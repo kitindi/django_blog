355f7f22270c9f9aae565e74a3677c787a369df4
Abdulaziz Sadi <abdullkitindi@gmail.com>
2023-03-20 15:44:41 +0300

    updated navbar and hero section bg color

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
